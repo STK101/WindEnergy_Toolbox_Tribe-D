783f171c1365510b39bd1e46dff91f0037b1afb4
Sajal Tyagi <60937901+STK101@users.noreply.github.com>
2022-01-29 14:19:47 +0530

    Added windspeed_from_data

--- utils.py
+++ utils.py
@@ -34,3 +34,20 @@ def voltageout(wind_speed, blade_length = 1):
 def monthly_output(average_wind_speeds):
     hourly_energy = [(poweroutput(average_wind_speeds[i]) + poweroutput(average_wind_speeds[i+1])) / 2  for i in range(0,len(average_wind_speeds) - 1)]
     return 30 * sum(hourly_energy)/ 1000 # 30 * daily /1000 to get in units
+'''
+Hellmann Table
+location 	                                    α
+Unstable air above open water surface: 	    0.06
+Neutral air above open water surface: 	    0.10
+Unstable air above flat open coast: 	      0.11
+Neutral air above flat open coast: 	        0.16
+Stable air above open water surface: 	      0.27
+Unstable air above human inhabited areas: 	0.27
+Neutral air above human inhabited areas: 	  0.34
+Stable air above flat open coast: 	        0.40
+Stable air above human inhabited areas: 	  0.60 
+'''
+  
+def windspeed_from_data(wind_speed_at_h, height_original, correct_hellmann, height_new = 10):
+  return wind_speed_at_h/(pow((height_original/height_new),correct_hellmann ))
+
